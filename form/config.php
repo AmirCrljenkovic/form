4f745286f698598bb9c83d84fda86792c40d1de1
AmirCrljenkovic <90686960+AmirCrljenkovic@users.noreply.github.com>
2022-06-24 22:41:20 +0200

    test

---
+++ form/config.php
@@ -0,0 +1,11 @@
+<?php
+$config = [
+    'server' => 'localhost',
+    'database' => 'berichten',
+    'username' => 'root',
+    'password' => 'root',
+    'port' => 3306
+];
+
+return $config
+?>
---
+++ form/index.php
@@ -0,0 +1,45 @@
+<?php
+require 'functions.php';
+$connection = dbConnect();
+
+$result = $connection->query('SELECT * FROM `berichten`');
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>formulier</title>
+</head>
+<body>
+    <div class="container">
+        <h1>portfolio</h1>
+
+        <section class="projecten">
+            <?php foreach($result as $row): ?>
+
+            <article class="titels">
+                <h2><?php echo $row['titel']; ?></h2>
+                <figure class="foto"><img src="img/<?php echo $row['foto']; ?>" alt="project 1" width="300px" height="300px"></figure>
+                <header>
+                </header>
+                <p class="beschrijving"><?php echo $row['beschrijving']; ?></p>
+                <a class="button" href="details.php?id=<?php echo $row['id'];?>">Meer Info</a>
+            </article>
+
+            <?php endforeach; ?>
+
+        
+
+
+        </section>
+    </div>
+    
+</body>
+</html>
